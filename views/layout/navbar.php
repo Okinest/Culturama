10eaebaf470d414a26333d80493dbda8ac84ade4
theoalleaume <theo.alleaume024@gmail.com>
2025-09-13 19:09:10 +0200

    Update page titles and navbar branding for Culturama

--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -2,15 +2,16 @@
 ?>
 <nav class="flex justify-between items-center p-4 bg-gray-100">
     <a href="/" class="font-bold text-lg">
-        MonSite
+        Culturama
     </a>
     <div>
         <?php if (isset($_SESSION['user'])): ?>
+            <p class="text-blue-600">Bonjour, <?= $_SESSION['user'] ?></p>
             <a href="/logout.php" title="Déconnexion">
                 <img src="/assets/logout.svg" alt="Déconnexion" class="h-6">
             </a>
         <?php else: ?>
-            <a href="/login.php" class="text-blue-600 hover:underline">Connexion</a>
+            <a href="/user/form" class="text-blue-600 hover:underline">Connexion</a>
         <?php endif; ?>
     </div>
 </nav>
