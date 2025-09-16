cc20e9103c8aef07892b598219fd1c8b529ac081
theoalleaume <theo.alleaume024@gmail.com>
2025-09-16 21:42:24 +0200

    Refactor password strength validation to use a single regex pattern

--- models/User.php
+++ models/User.php
@@ -133,10 +133,6 @@ class User
 
     public static function isPasswordStrong(string $password): bool
     {
-        return strlen($password) >= 8 &&
-               preg_match('/[A-Z]/', $password) &&
-               preg_match('/[a-z]/', $password) &&
-               preg_match('/\d/', $password) &&
-               preg_match('/[\W_]/', $password);
+        return preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password);
     }
 }
